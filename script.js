4233b7476f2bfefb5c7bb9ae7703244a3eafbb44
rorylkd <rory.donaldson14@gmail.com>
2021-04-01 17:24:27 +0800

    Added more html sections, added actual questions/answers to JS object

--- script.js
+++ script.js
@@ -3,202 +3,38 @@
 
 var questions = [
       {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
+        question: "What is the initial value of a variable?",
+        answer: "undefined",
+        options: [
+          "undefined",
+          "1",
+          "0",
+          "null"
+        ]
+      },
+
+
+      {
+        question: "What does the pop() method do?",
+        answer: "Removes the last element of an array, and returns that element.",
+        options: [
+          "Removes the last element of an array.",
+          "Removes the first element of an array, and returns that element.",
+          "Removes the last element of an array, and returns that element.",
+          "Removes the last element of an array."
+        ]
       },
 
       {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
+        question: "What removes the 1st element of an array?",
+        answer: "shift() method",
+        options: [
+          "pop() method",
+          "bind() method",
+          "apply() method",
+          "shift() method"
+        ]
+      },  
+];
 
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
 
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-      
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-      
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-      
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-      
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      },
-
-      {
-        question: "",
-        a: "",
-        b: "",
-        c: "",
-        answer: ""
-      }  
-    ];

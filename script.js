7e2a9b1847dd7c07aa801368942f74a9df81df37
rorylkd <rory.donaldson14@gmail.com>
2021-04-02 20:03:14 +0800

    Added a makeQuestion function, added check answer function, changed some id names cause they were dumb and added some CSS to the question and choices

--- script.js
+++ script.js
@@ -9,16 +9,87 @@ const nameEl = document.getElementById("name");
 const submitEl = document.getElementById("submit-score");
 const highscoreSectionEl = document.getElementById("highscoreSection");
 
-const choiceEl = document.getElementById("choice");
-const choiceAEl = document.getElementById("A");
-const choiceBEl = document.getElementById("B");
-const choiceCEl = document.getElementById("C");
-const choiceDEl = document.getElementById("D");
+const choices = document.getElementById("choices");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const choiceD = document.getElementById("D");
+
+const timerEl = document.getElementById("timer");
 
 
 // Event listener to start the quiz 
 
 startbuttonEl.addEventListener("click", startQuiz);
+
+// Creating question array
+
+let questions = [
+  {
+    question: "What is the initial value of a variable?",
+    choiceA: "undefined",
+    choiceB: "1",
+    choiceC: "0", 
+    choiceD: "null",
+    correct: "A" 
+},
+
+
+  {
+    question: "What does the pop() method do?",
+    choiceA:  "Removes the last element of an array.",
+    choiceB:  "Removes the first element of an array, and returns that element.",
+    choiceC:  "Removes the last element of an array, and returns that element.",
+    choiceD:  "Removes the last element of an array.",
+    correct: "C"
+
+  },
+
+  {
+    question: "What removes the 1st element of an array?",
+    choiceA:  "pop() method",
+    choiceB:  "bind() method",
+    choiceC:  "apply() method",
+    choiceD:  "shift() method",
+    correct: "D"
+    
+  }  
+];
+
+
+
+// Creating variables
+
+const lastQuestion = questions.length -1;
+let currentQuestion = 0; 
+let score = 0;
+
+
+
+
+
+function makeQuestion () {
+  let q = questions[currentQuestion];
+
+  question.innerHTML = "<p>"+ q.question +"</p>";
+  choiceA.innerHTML = q.choiceA;
+  choiceB.innerHTML = q.choiceB;
+  choiceC.innerHTML = q.choiceC;
+  choiceD.innerHTML = q.choiceD;
+}
+
+
+// Function to check answer...to be written
+
+function checkAnswer(answer) {
+  if(answer == questions[currentQuestion].correct){
+        console.log("GOOD JOB");}
+  else{console.log("YOU'RE DUMB");}
+  
+
+}
+
+
 
 //Function used in event listener; hides intro section (incl. start button) & shows question section.
 
@@ -26,92 +97,10 @@ function startQuiz() {
   introSectionEl.style.display = "none";
   questionSectionEl.style.display = "block";
   console.log("Is this working");
+  makeQuestion();
 
 }
 
-// Function to check answer...to be written
 
-function checkAnswer() {}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var questions = [
-      {
-        question: "What is the initial value of a variable?",
-        answer: "undefined",
-        choiceA: "undefined",
-        choiceB: "1",
-        choiceC: "0", 
-        choiceD: "null" 
-},
-
-
-      {
-        question: "What does the pop() method do?",
-        answer: "Removes the last element of an array, and returns that element.",
-        choiceA:  "Removes the last element of an array.",
-        choiceB:  "Removes the first element of an array, and returns that element.",
-        choiceC:  "Removes the last element of an array, and returns that element.",
-        choiceD:  "Removes the last element of an array."
-  
-      },
-
-      {
-        question: "What removes the 1st element of an array?",
-        answer: "shift() method",
-        choiceA:  "pop() method",
-        choiceB:  "bind() method",
-        choiceC:  "apply() method",
-        choiceD:  "shift() method"
-        
-      }  
-];
 
 

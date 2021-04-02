8291fca233c05d4b57d4e4234ff2ce3195aeeb16
rorylkd <rory.donaldson14@gmail.com>
2021-04-02 11:59:31 +0800

    Added id's to relevant elements, added css to hide sections at page load, added element selectors (by ID), added event listener for start button, added startquiz function

--- script.js
+++ script.js
@@ -1,3 +1,84 @@
+// Selecting all the elements by ID. 
+
+const startbuttonEl = document.getElementById("startbutton");
+const questionSectionEl = document.getElementById("questionSection");
+const questionEl = document.getElementById("question");
+const introSectionEl = document.getElementById("introSection");
+const finalSectionEl = document.getElementById("finalSection");
+const nameEl = document.getElementById("name");
+const submitEl = document.getElementById("submit-score");
+const highscoreSectionEl = document.getElementById("highscoreSection");
+
+const choiceEl = document.getElementById("choice");
+const choiceAEl = document.getElementById("A");
+const choiceBEl = document.getElementById("B");
+const choiceCEl = document.getElementById("C");
+const choiceDEl = document.getElementById("D");
+
+
+// Event listener to start the quiz 
+
+startbuttonEl.addEventListener("click", startQuiz);
+
+//Function used in event listener; hides intro section (incl. start button) & shows question section.
+
+function startQuiz() {
+  introSectionEl.style.display = "none";
+  questionSectionEl.style.display = "block";
+  console.log("Is this working");
+
+}
+
+// Function to check answer...to be written
+
+function checkAnswer() {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -5,36 +86,32 @@ var questions = [
       {
         question: "What is the initial value of a variable?",
         answer: "undefined",
-        options: [
-          "undefined",
-          "1",
-          "0",
-          "null"
-        ]
-      },
+        choiceA: "undefined",
+        choiceB: "1",
+        choiceC: "0", 
+        choiceD: "null" 
+},
 
 
       {
         question: "What does the pop() method do?",
         answer: "Removes the last element of an array, and returns that element.",
-        options: [
-          "Removes the last element of an array.",
-          "Removes the first element of an array, and returns that element.",
-          "Removes the last element of an array, and returns that element.",
-          "Removes the last element of an array."
-        ]
+        choiceA:  "Removes the last element of an array.",
+        choiceB:  "Removes the first element of an array, and returns that element.",
+        choiceC:  "Removes the last element of an array, and returns that element.",
+        choiceD:  "Removes the last element of an array."
+  
       },
 
       {
         question: "What removes the 1st element of an array?",
         answer: "shift() method",
-        options: [
-          "pop() method",
-          "bind() method",
-          "apply() method",
-          "shift() method"
-        ]
-      },  
+        choiceA:  "pop() method",
+        choiceB:  "bind() method",
+        choiceC:  "apply() method",
+        choiceD:  "shift() method"
+        
+      }  
 ];
 
 
